4d83d903220da02c4dcd202e78927431263ea1f0
AubinManceau <aubinmanceau0@gmail.com>
2025-04-17 09:56:00 +0200

    connect all reservations to api

--- src/app/(dashboard)/candidatures/formations/page.jsx
+++ src/app/(dashboard)/candidatures/formations/page.jsx
@@ -1,22 +1,46 @@
+'use client';
+
 import { AppNavbar } from "@/components/navbar";
 import { DataTableCandidature } from "@/components/data/DataTableCandidature";
 import { EmptyData } from "@/components/data/EmptyData";
+import { useState, useEffect } from "react";
+import { getAllTrainingReservations } from "@/services/training";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function candidatureFormations() {
-  const users = [
-    {id: 1, name: "Aubin Manceau", email: "aubin.manceau@gmail.com", phone: "06 12 34 56 78", status: "accepted"},
-  ];
+  const [trainingReservations, setTrainingReservations] = useState([]);
+  const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      const fetchTrainingReservations = async () => {
+        try {
+          const dataTrainingReservations = await getAllTrainingReservations();
+          setTrainingReservations(dataTrainingReservations);
+
+        } catch (error) {
+            console.error("Erreur lors de la récupération des formations :", error);
+        } finally {
+            setLoading(false);
+        }
+      };
+      
+      fetchTrainingReservations();
+    }, []);
   
   return (
     <>
       <AppNavbar pageName="Candidatures pour formations"/>
       <div className="flex flex-col gap-6 p-8 pt-0 h-full">
-        {users.length > 0 ? (
-        <DataTableCandidature data={users}  />
-        ) : (
-          <EmptyData message="Aucune candidature trouvée." />
+        {loading && (
+          <Skeleton className="w-full h-full"></Skeleton>
+        )}
+        {!loading && (
+          trainingReservations.length > 0 ? (
+            <DataTableCandidature data={trainingReservations} />
+          ) : (
+            <EmptyData message="Aucune candidature trouvée." />
+          )
         )}
       </div>
     </>
   );
-}
+};
